908c4dfbabfa29be8eef7a83c02c01577d63c2db
evikary <daria_more10@mail.ru>
2024-01-18 22:07:54 +0300

    feat: write text 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('node:process');
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const { stdin, stdout } = process;
+
+stdout.write('Приветик!\n');
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit\n') {
+    handleExit();
+  }
+  output.write(data);
+});
+
+process.on('SIGINT', () => {
+  handleExit();
+});
+
+function handleExit() {
+  stdout.write('До встречи!\n');
+  process.exit();
+}
